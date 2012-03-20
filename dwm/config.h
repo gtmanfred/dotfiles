01c93151b18d8f734ca6851f252eeb498646da26
Daniel Wallace <daniel.wallace12@gmail.com>
2012-03-20 13:23:51 -0400

    added setnmaster to config.h and PKGBUILD

--- dwm/config.h
+++ dwm/config.h
@@ -128,7 +128,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	//{ MODKEY,                       XK_x,      setnmaster,     {.i = 1 } },
+	{ MODKEY,                       XK_x,      setnmaster,     {.i = 1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,			            XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
